666ada49a77abd5a703b5bb653d03628ca80ffc2
kstandbridge <prepotency@gmail.com>
2018-11-22 14:26:15 +0000

    deps and Sandbox project setup

--- src/Sandbox/Main.cpp
+++ src/Sandbox/Main.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
 
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello, world!" << std::endl;
+	if(SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		std::cout << "Error! SDL_Init: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
 
+	SDL_Window* window = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	if(window == nullptr)
+	{
+		std::cout << "Error! SDL_CreateWindow: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if(renderer == nullptr)
+	{
+		std::cout << "Error! SDL_CreateRenderer: " << SDL_GetError() << std::endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
+
+	bool isRunning = true;
+	while (isRunning)
+	{
+		SDL_Event event;
+		SDL_PollEvent(&event);
+
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			isRunning = false;
+			break;
+		default:
+			break;
+		}
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return EXIT_SUCCESS;
 }
